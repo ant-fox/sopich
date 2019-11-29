6c03d5139468f76efc83adaef1727363def3fa6c
ssmtf <ceres.valdid@mail.com>
2019-11-29 16:17:35 +0100

    start

--- src/audioutils.js
+++ src/audioutils.js
@@ -39,9 +39,11 @@ function creatorName( nodeName ){
     let tail = nodeName.substring(1)
     return `create${ head  }${ tail }`
 }
-function createWebAudioNode( ctx, { node, c = [], p = {}, f } ){
+function createWebAudioNode( ctx, { node, c = [], ap = {}, p = {}, f } ){
     // create with optional parameters
     const waNode = ctx[ creatorName( node )  ]( ...c  )
+    // set audio parameters
+    Object.keys( ap ).forEach( k => { waNode[ k ].value = ap[ k ] } )
     // set attributes
     Object.keys( p ).forEach( k => { waNode[ k ] = p[ k ] } )
     // apply custom f
@@ -90,7 +92,7 @@ function getWanOutputs( src, audioNodes, audioParams, modules, accum = [] ){
     return accum
 }
 function getWanInputs( src, audioNodes, audioParams, modules, accum = [] ){
-      if ( audioParams[ src ] ){
+    if ( audioParams[ src ] ){
         accum.push( src )    
     } else if ( audioNodes[ src ] ){
         accum.push( src )    
@@ -163,19 +165,33 @@ export function instanciateModule( ctx, module ){
             }
         })
     }
+    function connect( dst ){
+        module.outputs.forEach( srcadd => {
+            let srcs = getWanOutputs( srcadd, waNodes, moduleNodes ).map( x => waNodes[ x ] )
+            srcs.forEach( src => {
+                if ( dst instanceof AudioNode ){
+                    src.connect( dst )
+                } else {
+                    throw new Error('n/i')
+                }
+            })
+            //console.log( src )
+        })
+    }
     return {
         audioNodes : waNodes,
         audioParams : waParams,
         audioConnections : waConnections,
         modules : moduleNodes,
         connections : connections,
-        audioOutputs : getWanOutputs( '', waNodes, moduleNodes ).map( x => waNodes[ x ] ),
-        audioInputs : getWanInputs( '', waNodes, moduleNodes ).map( x => waNodes[ x ] ),
+        //audioOutputs : getWanOutputs( '', waNodes, moduleNodes ).map( x => waNodes[ x ] ),
+        //audioInputs : getWanInputs( '', waNodes, moduleNodes ).map( x => waNodes[ x ] ),
         start : () => {
             doInnerConnections()
             doStart()
         },
-        audioParam : path =>  waParams[ path ]
+        audioParam : path =>  waParams[ path ],
+        audioNode : path =>  waNodes[ path ],connect
     }
 
 }
@@ -192,118 +208,118 @@ async function examples(  ){
 
         const model = {
             nodes : {
-                osc1 : { node : 'oscillator' },
-                osc2 : { node : 'oscillator' },
+                osc1 : { node : 'oscillator', ap : { frequency : 400 }},
+                osc2 : { node : 'oscillator', ap : { frequency : 20 }},
                 gain : { node : 'gain' },
             },
             connections : [
                 ['osc1','gain'],
                 ['osc2','gain/gain'],
-            ]
+            ],
+            outputs : [ 'gain' ]
         }
         let m = instanciateModule( ctx, model )
-        m.audioNodes['gain'].connect( ctx.destination )
-        console.log( m )
+        
+        m.audioParam('osc2/frequency').linearRampToValueAtTime(0, ctx.currentTime + 5)
+        m.connect( ctx.destination )
         setTimeout( () => {
             m.audioNodes['gain'].disconnect( ctx.destination )
-        },1000)
-        m.audioParam('osc1/frequency').value = 500
-        m.audioParam('osc2/frequency').value = 20
+        },5000)
         m.start()
     }
     function example1(){
-    function monoWhiteNoiseBuffer( duration ){
-        const bufferSize = ctx.sampleRate * duration
-        const buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate)
-        let data = buffer.getChannelData(0)
-        for (let i = 0; i < bufferSize; i++) {
-            data[i] = Math.random() * 2 - 1;
+        function monoWhiteNoiseBuffer( duration ){
+            const bufferSize = ctx.sampleRate * duration
+            const buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate)
+            let data = buffer.getChannelData(0)
+            for (let i = 0; i < bufferSize; i++) {
+                data[i] = Math.random() * 2 - 1;
+            }
+            return buffer
         }
-        return buffer
-    }
 
-    function setPeriodicWavef( wavetable ){
-        const wave = ctx.createPeriodicWave(
-            new Float32Array(wavetable.real),
-            new Float32Array(wavetable.imag)
-        )
-        return function( node ){
-            node.setPeriodicWave( wave )
+        function setPeriodicWavef( wavetable ){
+            const wave = ctx.createPeriodicWave(
+                new Float32Array(wavetable.real),
+                new Float32Array(wavetable.imag)
+            )
+            return function( node ){
+                node.setPeriodicWave( wave )
+            }
         }
-    }
-    function setupCompressor( dynamicsCompressorNode ){
-        const c = dynamicsCompressorNode
-        c.knee.setValueAtTime(40, ctx.currentTime);  // [ 0, 40 ] 30
-        c.ratio.setValueAtTime(12, ctx.currentTime); // [ 1, 20 ] 12
-        c.attack.setValueAtTime(0, ctx.currentTime);
-    }
-    const periodicWaveOscillatorGain = {
-        nodes : {
-            osc : { node : 'oscillator' , f : setPeriodicWavef( wavetable ) },
-            gain : { node : 'gain' }
-        },
-        connections : [ ['osc','gain'] ],
-        outputs : [ 'gain' ]    
-    }
-    const whiteNoiseBandPassGain = {
-        nodes : {
-            whitenoise : { node : 'bufferSource', p : {
-                buffer : monoWhiteNoiseBuffer(1),
-                loop : true
-            }},
-            bandpass : { node : 'biquadFilter', p : { type : 'bandpass' } },
-            gain : { node : 'gain' },
-        },
-        connections: [['whitenoise','bandpass','gain']],
-        outputs : ['gain']
-    }
-    const compressorGain = {
-        nodes : {
-            pre : { node : 'gain' },
-            compressor : { node : 'dynamicsCompressor', f : setupCompressor },
-            post : { node : 'gain' },
-        },
-        connections: [['pre','compressor','post']],
-        inputs : [ 'pre'],
-        outputs : [ 'post' ]
-    }
-    const brahoum = {
-        nodes : {
-            noise : { module : whiteNoiseBandPassGain },
-            compression : { module : compressorGain }
-        },
-        connections : [['noise','compression']],
-        outputs : ['compression']
-    }
+        function setupCompressor( dynamicsCompressorNode ){
+            const c = dynamicsCompressorNode
+            c.knee.setValueAtTime(40, ctx.currentTime);  // [ 0, 40 ] 30
+            c.ratio.setValueAtTime(12, ctx.currentTime); // [ 1, 20 ] 12
+            c.attack.setValueAtTime(0, ctx.currentTime);
+        }
+        const periodicWaveOscillatorGain = {
+            nodes : {
+                osc : { node : 'oscillator' , f : setPeriodicWavef( wavetable ) },
+                gain : { node : 'gain' }
+            },
+            connections : [ ['osc','gain'] ],
+            outputs : [ 'gain' ]    
+        }
+        const whiteNoiseBandPassGain = {
+            nodes : {
+                whitenoise : { node : 'bufferSource', p : {
+                    buffer : monoWhiteNoiseBuffer(1),
+                    loop : true
+                }},
+                bandpass : { node : 'biquadFilter', p : { type : 'bandpass' } },
+                gain : { node : 'gain' },
+            },
+            connections: [['whitenoise','bandpass','gain']],
+            outputs : ['gain']
+        }
+        const compressorGain = {
+            nodes : {
+                pre : { node : 'gain' },
+                compressor : { node : 'dynamicsCompressor', f : setupCompressor },
+                post : { node : 'gain' },
+            },
+            connections: [['pre','compressor','post']],
+            inputs : [ 'pre'],
+            outputs : [ 'post' ]
+        }
+        const brahoum = {
+            nodes : {
+                noise : { module : whiteNoiseBandPassGain },
+                compression : { module : compressorGain }
+            },
+            connections : [['noise','compression']],
+            outputs : ['compression']
+        }
 
-    /*
-      const mixer2module = {
-      nodes : {
-      in1 : { node : 'gain' },
-      in2 : { node : 'gain' }
-      },
-      outputs : ['in1','in2'],
-      }  
-    */
-    let m = instanciateModule( ctx, brahoum )
-    m.audioOutputs.forEach( wan => wan.connect( ctx.destination ) )
-    m.start()
-    console.log( Object.keys( m.audioParams ).join("\n") )
-    m.audioParam('compression.post/gain').linearRampToValueAtTime(0, ctx.currentTime + 5)
-    m.audioParam('noise.bandpass/Q').linearRampToValueAtTime(0, ctx.currentTime + 5)
-    m.audioParam('noise.bandpass/frequency').linearRampToValueAtTime(4000, ctx.currentTime + 5)
-    
-    // occurs between previous scheduled event and <endTime>
-    // var AudioParam = AudioParam.linearRampToValueAtTime(value, endTime)
-    // var AudioParam = AudioParam.exponentialRampToValueAtTime(value, endTime)
-    
-    // occurs at <startTime>
-    // var AudioParam = AudioParam.setValueAtTime(value, startTime)
-    // var AudioParam = AudioParam.setTargetAtTime(target, startTime, timeConstant);  
-    // var AudioParam = AudioParam.setValueCurveAtTime(values, startTime, duration);
-    
-    // var AudioParam = AudioParam.cancelScheduledValues(startTime)
-    // var audioParam = AudioParam.cancelAndHoldAtTime(cancelTime)
+        /*
+          const mixer2module = {
+          nodes : {
+          in1 : { node : 'gain' },
+          in2 : { node : 'gain' }
+          },
+          outputs : ['in1','in2'],
+          }  
+        */
+        let m = instanciateModule( ctx, brahoum )
+        m.audioOutputs.forEach( wan => wan.connect( ctx.destination ) )
+        m.start()
+        console.log( Object.keys( m.audioParams ).join("\n") )
+        m.audioParam('compression.post/gain').linearRampToValueAtTime(0, ctx.currentTime + 5)
+        m.audioParam('noise.bandpass/Q').linearRampToValueAtTime(0, ctx.currentTime + 5)
+        m.audioParam('noise.bandpass/frequency').linearRampToValueAtTime(4000, ctx.currentTime + 5)
+        
+        // occurs between previous scheduled event and <endTime>
+        // var AudioParam = AudioParam.linearRampToValueAtTime(value, endTime)
+        // var AudioParam = AudioParam.exponentialRampToValueAtTime(value, endTime)
+        
+        // occurs at <startTime>
+        // var AudioParam = AudioParam.setValueAtTime(value, startTime)
+        // var AudioParam = AudioParam.setTargetAtTime(target, startTime, timeConstant);  
+        // var AudioParam = AudioParam.setValueCurveAtTime(values, startTime, duration);
+        
+        // var AudioParam = AudioParam.cancelScheduledValues(startTime)
+        // var audioParam = AudioParam.cancelAndHoldAtTime(cancelTime)
     }
     example2()
 }
@@ -321,4 +337,4 @@ export function fetchWaveTable( url ){
 }
 
 
-examples()
+// examples()
