8517433d03c50e7fdaa92697c8572444295ef472
ssmtf <ceres.valdid@mail.com>
2019-12-01 18:08:58 +0100

    cpu have names

--- src/game.js
+++ src/game.js
@@ -61,6 +61,46 @@ export const PLANE_INPUT_NAMES = [
     'powerup','powerdown',
     'firebomb','firemissile'
 ]
+function buildName(){
+    let letters = [
+        /* rare wov */ 'jy'.split(''),
+        /* freq wov */ 'aeiou'.split(''),
+        /* rare_con */ 'hkqvxz'.split(''),
+        /* freq_con */ 'bcdfglmnprst'.split('')
+    ]    
+    let freqs = [
+        // rw fw rc fc
+        [ 0,1,1,10 ], // rw
+        [ 1,4,4,8 ], // fw
+        [ 1,20,0,0 ], // rc
+        [ 2,20,1,1 ]  // fc
+    ]
+    let arrays = freqs.map( (freqs,fidx) => {
+        let total = freqs.reduce( (r,x) => r+x, 0 )
+        let normalized = freqs.map( x => x/total)
+        let minArraySize = 20
+        let sized = normalized.map( x => {
+            return ( x === 0 )?0:Math.ceil( x * minArraySize )
+        })
+        let array = sized.reduce( ( r, x, idx ) => {
+            return [...r, ...new Array( x ).fill( idx ) ]
+        },[])
+        return array
+    })
+    let length = 4 + Math.floor( Math.pow( Math.random(), 5 ) * 10 )
+    let last = Math.floor( Math.random() * letters.length )
+    let namea = []
+    for ( let i = 0 ; i < length ; i++ ){
+        let array = arrays[ last ]
+        let rndIdx = Math.floor( Math.random() * array.length )
+        let categ = array[ rndIdx ]
+        let choices = letters[ categ ]
+        let letter = choices[ Math.floor( Math.random() * choices.length ) ]
+        namea[ i ] = letter     
+        last = categ
+    }
+    return namea.join('')
+}
 /*
  *
  */
@@ -329,7 +369,8 @@ export function Game( { tellPlayer, // called with user centered world, each wor
             leaving : init_leaving_plane(),
             respawn : -1,
             value : 10,
-            score : init_score( idx )
+            score : init_score( idx ),
+            defaultname : buildName()
         }
     }
     function init_state(){
@@ -1153,14 +1194,20 @@ export function Game( { tellPlayer, // called with user centered world, each wor
             let { x, y, as } = ox
             payload.oxs.push( { x, y, as } )
         })
+        
         State.planes.forEach( plane => {
-            let { ttl, x, y, r, a, p, cs, explosion, leaving, falling, value, score } = plane
+            let { defaultname,
+                  idx, ttl, x, y, r, a, p, cs, explosion, leaving, falling, value, score } = plane
             let name = '??'
             if ( plane.inputId ){
                 const player = playerByInputId[ plane.inputId ]
-                
-                // name = nameByInputId[ plane.inputId ]
-                name = player.name
+                if ( player && player.name ){
+                    name = player.name
+                }
+            } else {
+                if ( defaultname )  {
+                    name = defaultname
+                }
             }
             // TODO
             //if ( ttl > 0 ){
