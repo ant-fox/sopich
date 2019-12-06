b3cc9205d4bca90b4bdeb62fe18b7def3256af1f
ssmtf <ceres.valdid@mail.com>
2019-12-06 16:05:28 +0100

    ...

--- src/iogame/server/user.js
+++ src/iogame/server/user.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-
-
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: String,
@@ -8,7 +6,6 @@ const UserSchema = new Schema({
     passwordHash : String
 })
 UserSchema.statics.findByUsername = function(username) {
-    //return this.find({ username: new RegExp(username, 'i') });
     return this.find({ username })
 };
 export const User = mongoose.model('User', UserSchema);
