88eaa1da92dcddaddb864d4da4a846f223da2269
ssmtf <ceres.valdid@mail.com>
2020-04-26 14:32:24 +0200

    remove unused constants

--- src/iogame/shared/constants.js
+++ src/iogame/shared/constants.js
@@ -1,17 +1,5 @@
 module.exports = Object.freeze({
-  PLAYER_RADIUS: 20,
-  PLAYER_MAX_HP: 100,
-  PLAYER_SPEED: 400,
-  PLAYER_FIRE_COOLDOWN: 0.25,
-
-  BULLET_RADIUS: 3,
-  BULLET_SPEED: 800,
-  BULLET_DAMAGE: 10,
-
-  SCORE_BULLET_HIT: 20,
-  SCORE_PER_SECOND: 1,
-
-  MAP_SIZE: 3000,
+    MAP_SIZE: 3000,
     MSG_TYPES: {
         YOUR_INFO : 'your_info',
         JOINED_GAME_OK : 'joined_game_ok',
