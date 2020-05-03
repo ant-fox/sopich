cc5dfa5ccdbb2ff20d954aab847634af6ab1a471
ssmtf <ceres.valdid@mail.com>
2020-05-03 14:40:40 +0200

    restore display & sound

--- src/iogame/client/render.js
+++ src/iogame/client/render.js
@@ -4,10 +4,17 @@ import { Audio } from '../../../src/audio.js'
 import { debounce } from 'throttle-debounce';
 import { getCurrentState } from './state';
 
+const NO_RENDER = false
+const INTERVAL_RENDER = false
+
 // display 
 const display = new Display()
-setInterval( () => display.display(), 1000 )
-// display.animate()
+if ( NO_RENDER ){
+} else if (  INTERVAL_RENDER ){
+    setInterval( () => display.display(), 1000 )
+} else {
+    display.animate()
+}
 
 // sound
 const audio = new Audio()
@@ -28,18 +35,28 @@ function renderMainMenu() {
  */
 }
 
-let renderInterval = setInterval(renderMainMenu, 1000 / 60);
+let renderInterval
+if ( NO_RENDER ){
+} else {
+    renderInterval = setInterval(renderMainMenu, 1000 / 60);
+}
 
 // Replaces main menu rendering with game rendering.
 export function startRendering() {
-    audio.start()
-    clearInterval(renderInterval);
-    renderInterval = setInterval(render, 1000 / 60);    
+    if ( NO_RENDER ){
+    } else {
+        audio.start()
+        clearInterval(renderInterval);
+        renderInterval = setInterval(render, 1000 / 60);
+    }
 }
 
 // Replaces game rendering with main menu rendering.
 export function stopRendering() {
-    audio.stop()
-  clearInterval(renderInterval);
-  renderInterval = setInterval(renderMainMenu, 1000 / 60);
+    if ( NO_RENDER ){
+    } else {
+        audio.stop()
+        clearInterval(renderInterval);
+        renderInterval = setInterval(renderMainMenu, 1000 / 60);
+    }
 }
